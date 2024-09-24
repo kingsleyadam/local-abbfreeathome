4281d9ad066833997722646ab24e46ed0e5c825e
Adam Kingsley <akingsley@linkedin.com>
2024-09-24 23:21:32 +0200

    Use pytest

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,17 +1,19 @@
-"""Unit test for Base device."""
+"""Test class to test the Base device."""
 
-import unittest
 from unittest.mock import MagicMock
+
+import pytest
 
 from abbfreeathome.api import FreeAtHomeApi
 from abbfreeathome.devices.base import Base
 from abbfreeathome.exceptions import InvalidDeviceChannelPairingId
 
 
-class TestBase(unittest.TestCase):
+class TestBase:
     """The TestBase class for testing the Base device."""
 
-    def setUp(self):
+    @pytest.fixture(autouse=True)
+    def _setup(self):
         """Set up the test class."""
         self.api = MagicMock(spec=FreeAtHomeApi)
         self.device_id = "device123"
@@ -40,51 +42,47 @@ class TestBase(unittest.TestCase):
 
     def test_device_id(self):
         """Test the device id."""
-        self.assertEqual(self.base.device_id, self.device_id)
+        assert self.base.device_id == self.device_id
 
     def test_device_name(self):
         """Test the device name."""
-        self.assertEqual(self.base.device_name, self.device_name)
+        assert self.base.device_name == self.device_name
 
     def test_channel_id(self):
         """Test the channel id."""
-        self.assertEqual(self.base.channel_id, self.channel_id)
+        assert self.base.channel_id == self.channel_id
 
     def test_channel_name(self):
         """Test the channel name."""
-        self.assertEqual(self.base.channel_name, self.channel_name)
+        assert self.base.channel_name == self.channel_name
 
     def test_get_input_by_pairing_id(self):
         """Test the get_input_by_paring_id function."""
         input_id, value = self.base.get_input_by_pairing_id(1)
-        self.assertEqual(input_id, "input1")
-        self.assertEqual(value, "input_value1")
+        assert input_id == "input1"
+        assert value == "input_value1"
 
-        with self.assertRaises(InvalidDeviceChannelPairingId):
+        with pytest.raises(InvalidDeviceChannelPairingId):
             self.base.get_input_by_pairing_id(99)
 
     def test_get_output_by_pairing_id(self):
         """Test the get_output_by_pairing_id function."""
         output_id, value = self.base.get_output_by_pairing_id(1)
-        self.assertEqual(output_id, "output1")
-        self.assertEqual(value, "output_value1")
+        assert output_id == "output1"
+        assert value == "output_value1"
 
-        with self.assertRaises(InvalidDeviceChannelPairingId):
+        with pytest.raises(InvalidDeviceChannelPairingId):
             self.base.get_output_by_pairing_id(99)
 
     def test_register_callback(self):
         """Test register a callback."""
         callback = MagicMock()
         self.base.register_callback(callback)
-        self.assertIn(callback, self.base._callbacks)  # noqa: SLF001
+        assert callback in self.base._callbacks  # noqa: SLF001
 
     def test_remove_callback(self):
         """Test removing a callback."""
         callback = MagicMock()
         self.base.register_callback(callback)
         self.base.remove_callback(callback)
-        self.assertNotIn(callback, self.base._callbacks)  # noqa: SLF001
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert callback not in self.base._callbacks  # noqa: SLF001
