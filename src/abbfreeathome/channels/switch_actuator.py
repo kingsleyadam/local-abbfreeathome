9306d24ca9e60a48f85f9f28288c2fe85fd85e99
Joerg Schoppet <joerg@schoppet.de>
2025-07-22 10:05:51 +0200

    Implement Welcome IP Mute Actuator/Sensor

--- src/abbfreeathome/channels/switch_actuator.py
+++ src/abbfreeathome/channels/switch_actuator.py
@@ -19,7 +19,81 @@ class SwitchActuatorForcedPosition(enum.Enum):
     forced_off = "5"
 
 
-class SwitchActuator(Base):
+class WelcomeIPMuteActuator(Base):
+    """Free@Home WelcomeIPMuteActuator Class."""
+
+    _state_refresh_pairings: list[Pairing] = [
+        Pairing.AL_INFO_ON_OFF,
+    ]
+    _callback_attributes: list[str] = [
+        "state",
+    ]
+
+    def __init__(
+        self,
+        device: "Device",
+        channel_id: str,
+        channel_name: str,
+        inputs: dict[str, dict[str, Any]],
+        outputs: dict[str, dict[str, Any]],
+        parameters: dict[str, dict[str, Any]],
+        floor_name: str | None = None,
+        room_name: str | None = None,
+    ) -> None:
+        """Initialize the Free@Home WelcomeIPMuteActuator class."""
+        self._state: bool | None = None
+
+        super().__init__(
+            device,
+            channel_id,
+            channel_name,
+            inputs,
+            outputs,
+            parameters,
+            floor_name,
+            room_name,
+        )
+
+    @property
+    def state(self) -> bool | None:
+        """Get the state of the switch."""
+        return self._state
+
+    async def turn_on(self):
+        """Turn on the switch."""
+        await self._set_switching_datapoint("1")
+        self._state = True
+
+    async def turn_off(self):
+        """Turn on the switch."""
+        await self._set_switching_datapoint("0")
+        self._state = False
+
+    def _refresh_state_from_datapoint(self, datapoint: dict[str, Any]) -> str:
+        """
+        Refresh the state of the channel from a given output.
+
+        This will return the name of the attribute, which was refreshed or None.
+        """
+        if datapoint.get("pairingID") == Pairing.AL_INFO_ON_OFF.value:
+            self._state = datapoint.get("value") == "1"
+            return "state"
+        return None
+
+    async def _set_switching_datapoint(self, value: str):
+        """Set the switching datapoint on the api."""
+        _switch_input_id, _switch_input_value = self.get_input_by_pairing(
+            pairing=Pairing.AL_SWITCH_ON_OFF
+        )
+        return await self.device.api.set_datapoint(
+            device_serial=self.device_serial,
+            channel_id=self.channel_id,
+            datapoint=_switch_input_id,
+            value=value,
+        )
+
+
+class SwitchActuator(WelcomeIPMuteActuator):
     """Free@Home SwitchActuator Class."""
 
     _state_refresh_pairings: list[Pairing] = [
@@ -43,7 +117,6 @@ class SwitchActuator(Base):
         room_name: str | None = None,
     ) -> None:
         """Initialize the Free@Home SwitchActuator class."""
-        self._state: bool | None = None
         self._forced_position: SwitchActuatorForcedPosition = (
             SwitchActuatorForcedPosition.unknown
         )
@@ -60,24 +133,9 @@ class SwitchActuator(Base):
         )
 
     @property
-    def state(self) -> bool | None:
-        """Get the state of the switch."""
-        return self._state
-
-    @property
     def forced_position(self) -> str | None:
         """Get the forced state of the switch."""
         return self._forced_position.name
-
-    async def turn_on(self):
-        """Turn on the switch."""
-        await self._set_switching_datapoint("1")
-        self._state = True
-
-    async def turn_off(self):
-        """Turn on the switch."""
-        await self._set_switching_datapoint("0")
-        self._state = False
 
     async def set_forced_position(self, forced_position_name: str):
         """Set the forced-option on the switch."""
@@ -101,9 +159,10 @@ class SwitchActuator(Base):
 
         This will return the name of the attribute, which was refreshed or None.
         """
-        if datapoint.get("pairingID") == Pairing.AL_INFO_ON_OFF.value:
-            self._state = datapoint.get("value") == "1"
-            return "state"
+        _return_value = super()._refresh_state_from_datapoint(datapoint)
+        if _return_value is not None:
+            return _return_value
+
         if datapoint.get("pairingID") == Pairing.AL_INFO_FORCE.value:
             try:
                 self._forced_position = SwitchActuatorForcedPosition(
@@ -113,18 +172,6 @@ class SwitchActuator(Base):
                 self._forced_position = SwitchActuatorForcedPosition.unknown
             return "forced_position"
         return None
-
-    async def _set_switching_datapoint(self, value: str):
-        """Set the switching datapoint on the api."""
-        _switch_input_id, _switch_input_value = self.get_input_by_pairing(
-            pairing=Pairing.AL_SWITCH_ON_OFF
-        )
-        return await self.device.api.set_datapoint(
-            device_serial=self.device_serial,
-            channel_id=self.channel_id,
-            datapoint=_switch_input_id,
-            value=value,
-        )
 
     async def _set_force_datapoint(self, value: str):
         """Set the force datapoint on the api."""
